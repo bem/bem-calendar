0a3e088f8b78b3fdaf668b77e916935e86d6d514
Vladimir Grinenko <i@tadatuta.com>
2017-02-19 03:42:55 +0300

    Improve focus

--- common.blocks/input/_has-calendar/input_has-calendar.js
+++ common.blocks/input/_has-calendar/input_has-calendar.js
@@ -15,17 +15,27 @@ provide(Input.declMod({ modName: 'has-calendar', modVal: true }, /** @lends inpu
             inited: function() {
                 this.__base.apply(this, arguments);
 
+                this._needShowCalendar = true;
+
                 this._calendar = this.findChildBlock(Calendar)
                     .setVal(this.getVal())
                     .setAnchor(this.domElem);
 
                 this._events(this._calendar).on('change', function(e, data) {
-                    this.setVal(data.formated);
+                    this._needShowCalendar = false;
+
+                    this
+                        .setVal(data.formated)
+                        .setMod('focused');
                 });
 
                 this._domEvents(bemDom.doc).on('pointerdown', function(e) {
                     var target = $(e.target),
                         insideCalendar = dom.contains(this._calendar.domElem, target);
+
+                    if(!insideCalendar && e.target !== this._elem('calendar').domElem[0]) {
+                        this._needShowCalendar = true;
+                    }
 
                     this._ignoreBlur = insideCalendar;
                 });
@@ -39,11 +49,14 @@ provide(Input.declMod({ modName: 'has-calendar', modVal: true }, /** @lends inpu
             'true': function() {
                 this.__base.apply(this, arguments);
 
+                if(!this._needShowCalendar) {
+                    this._needShowCalendar = true;
+                    return;
+                }
+
                 if(this._calendar.isShown()) return;
 
-                this._calendar
-                    .setVal(this.getVal())
-                    .show();
+                this._showCalendar();
             },
             '': function() {
                 this.__base.apply(this, arguments);
@@ -56,18 +69,34 @@ provide(Input.declMod({ modName: 'has-calendar', modVal: true }, /** @lends inpu
             }
         }
     },
-    onCalendarClick: function() {
-        if(this._calendar.isShown()) {
-            this._calendar.hide();
-        } else {
-            this._calendar
-                .setVal(this.getVal())
-                .show();
+    _showCalendar: function() {
+        this._calendar
+            .setVal(this.getVal())
+            .show();
+
+        return this;
+    },
+    _onPointerclick: function(e) {
+        this.__base.apply(this, arguments);
+
+        if(this.hasMod('disabled') || this._calendar.isShown()) return;
+
+        if(e.target === this._elem('calendar').domElem[0]) {
+            this._needShowCalendar = true;
+            return;
         }
+
+        this._showCalendar();
+    },
+    _onCalendarClick: function() {
+        this
+            .setMod('focused')
+            ._needShowCalendar = false;
     }
 }, {
     onInit: function() {
-        this._domEvents('calendar').on('pointerclick', this.prototype.onCalendarClick);
+        this._domEvents().on('pointerclick', this.prototype._onPointerclick);
+        this._domEvents('calendar').on('pointerclick', this.prototype._onCalendarClick);
 
         this.__base.apply(this, arguments);
     }
