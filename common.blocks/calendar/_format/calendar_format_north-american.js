3d4165d79fb64859f719aba8ba83833e14c375b4
AsTex <v.milenko@innosoft.pro>
2017-02-19 03:21:34 +0300

    Deps fix

--- common.blocks/calendar/_format/calendar_format_north-american.js
+++ common.blocks/calendar/_format/calendar_format_north-american.js
@@ -2,14 +2,15 @@ modules.define('calendar', function(provide, Calendar) {
 
     provide(Calendar.declMod({ modName: 'format', modVal: 'north-american' }, {
         _formatDate: function(date) {
-
             var month = date.getMonth() + 1,
                 day = date.getDate();
+
             return [month < 10 ? '0' + month : month, day < 10 ? '0' + day : day, date.getFullYear()].join('/');
         },
         _parseDateParts: function() {
             var date = this.__base.apply(this, arguments);
             if(date) {
+
                 return {
                     day: date.month + 1,
                     month: date.day - 1,
@@ -26,6 +27,7 @@ modules.define('calendar', function(provide, Calendar) {
                 week = new Array(countDays);
                 week[0] = new Date(dateIterator.getTime());
             }
+
             return { week: week, weekDay: weekDay };
         },
         _isWeekend: function(dayNumber) {
