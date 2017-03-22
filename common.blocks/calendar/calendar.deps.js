d6d0188c2f5d5841f0e26176ba40284f41023eb3
rteamx <rteamx@yandex.ru>
2017-03-22 08:25:15 +0700

    Remove theme deps

--- common.blocks/calendar/calendar.deps.js
+++ common.blocks/calendar/calendar.deps.js
@@ -8,9 +8,6 @@
                 elems: ['arrow', 'day', 'dayname']
             },
             {
-                mods: { theme: 'islands' }
-            },
-            {
                 block: 'popup',
                 mods: {
                     theme: 'islands',
