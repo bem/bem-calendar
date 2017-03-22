7638ede56d71d87efabf340d390fb5910c5cbc45
rteamx <rteamx@yandex.ru>
2017-03-22 08:22:44 +0700

    Change bemhtml

--- common.blocks/calendar/calendar.bemhtml.js
+++ common.blocks/calendar/calendar.bemhtml.js
@@ -1,13 +1,10 @@
-block('calendar')(
-    mix()(function() {
-        var mix = { block: 'popup' };
-
-        mix.mods = {
+block('calendar').mix()(function() {
+    return {
+        block: 'popup',
+        mods: {
             'has-calendar': true,
             target: 'anchor',
             theme: this.mods.theme
-        };
-
-        return mix;
-    })
-);
+        }
+    };
+});
