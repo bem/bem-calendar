67d99796a056115069c20724dae7e88752b4600d
d3m1d0v <y.a.demidov@yandex.ru>
2018-08-07 15:39:02 +0300

    Support bem-xjst 7.*

--- common.blocks/calendar/calendar.bemhtml.js
+++ common.blocks/calendar/calendar.bemhtml.js
@@ -1,5 +1,8 @@
 block('calendar')(
-    addJs()(function() {
-        return { val: this.ctx.val };
+    js()(function() {
+        var prevJs = applyNext(),
+            js = { val: this.ctx.val };
+
+        return prevJs ? this.extend(prevJs, js) : js;
     })
 );
